b1215a298bc8847e1d5c1761ac7a81e92321100a
bataevoff <85867199+bataevoff@users.noreply.github.com>
2022-07-22 04:55:20 +0300

    added Hero component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
 import GlobalStyles from './globalStyles';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <Navbar />
+      <Hero />
     </>
   );
 }
