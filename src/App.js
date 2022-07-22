fdcb69dec7fbc982019b0ea714683f5d688af23a
bataevoff <85867199+bataevoff@users.noreply.github.com>
2022-07-22 05:24:32 +0300

    added some custom styled components and react icons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Navbar from './components/Navbar';
 import GlobalStyles from './globalStyles';
 import Hero from './components/Hero';
+import { SliderData } from './data/SliderData';
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <Navbar />
-      <Hero />
+      <Hero slides={SliderData}/>
     </>
   );
 }
