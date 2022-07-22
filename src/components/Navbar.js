f0520647d01fa2d4f8d0e5842f92821efc6e21c9
bataevoff <85867199+bataevoff@users.noreply.github.com>
2022-07-22 04:50:51 +0300

    added custom svg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,8 @@ import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom'
 import { menuData } from '../data/MenuData';
 import { Button } from './Button';
+import Bars from '../images/bars.svg'
+import { FaBars } from 'react-icons/fa';
 
 const Nav = styled.nav`
   height: 60px;
@@ -30,14 +32,38 @@ const Logo = styled(Link)`
   font-style: italic;
 `
 
+// const MenuBars = styled(FaBars)`
+//   display: none;
+//
+//   @media screen and (max-width: 768px) {
+//     display: block;
+//   }
+// `
 const MenuBars = styled.i`
   display: none;
+  
+  @media screen and (max-width: 768px) {
+    display: block;
+    background-image: url(${Bars});
+    background-size: contain;
+    height: 40px;
+    width: 40px;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-50%, 25%);
+  }
 `
 
 const NavMenu = styled.div`
   display: flex;
   align-items: center;
   margin-right: -48px;
+  
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `
 
 const NavMenuLinks = styled(Link)`
@@ -48,6 +74,10 @@ const NavBtn = styled.div`
   display: flex;
   align-items: center;
   margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `
 
 const Navbar = () => {
