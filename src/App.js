f7bfa0769310a0da403f1eea7fb2a513656215ec
bataevoff <85867199+bataevoff@users.noreply.github.com>
2022-07-22 06:10:09 +0300

    added Dropdown component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navbar from './components/Navbar';
 import GlobalStyles from './globalStyles';
 import Hero from './components/Hero';
 import { SliderData } from './data/SliderData';
+import Dropdown from './components/Dropdown';
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <Navbar />
+      <Dropdown />
       <Hero slides={SliderData}/>
     </>
   );
---
+++ src/components/Dropdown.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import { menuData } from '../data/MenuData';
+import { Button } from './Button';
+
+const DropdownContainer = styled.div`
+  position: fixed;
+  z-index: 999;
+  width: 100%;
+  height: 100%;
+  background: #cd853f;
+  display: grid;
+  align-items: center;
+  top: 0;
+  left: 0;
+  transition: 0.3s ease-in-out;
+  opacity: 1;
+`
+
+const Icon = styled.div``
+const Icon = styled.div``
+const Icon = styled.div``
+const Icon = styled.div``
+const Icon = styled.div``
+const Icon = styled.div``
+
+const Dropdown = () => {
+  return (
+    <DropdownContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <DropdownWrapper>
+        <DropdownMenu>
+          {menuData.map((item, index) => (
+            <DropdownLink to={item.link} key={index}>
+              {item.title}
+            </DropdownLink>
+          ))}
+        </DropdownMenu>
+        <BtnWrap>
+          <Button primary='true' round='true' big='true' to='/contact'>
+            Contact Us
+          </Button>
+        </BtnWrap>
+      </DropdownWrapper>
+    </DropdownContainer>
+  );
+};
+
+export default Dropdown;
